b17337b3aa5d8dce3ae9ee261e3f55b2bec5632e
wqyeo <yeo_dev@protonmail.com>
2024-06-08 19:20:53 +0800

    API route to get file by UUID

---
+++ steganography_api/api/routers/get_file.py
@@ -0,0 +1,18 @@
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404
+from ..models import FileModel
+
+def download_file(request):
+    if request.method != 'GET':
+        return JsonResponse({'status': 'ERROR'}, status=404)
+
+    try:
+        file_uuid = request.GET['file_uuid']
+        file_model = get_object_or_404(FileModel, id=file_uuid)
+        file = file_model.file
+        response = FileResponse(file)
+        response['Content-Disposition'] = 'attachment; filename=' + file_model.file_name
+        return response
+    except Exception as e:
+        print(f"Error occurred downloading file :: {e}", flush=True)
+        return JsonResponse({'status': 'ERROR'}, status=500)
--- steganography_api/api/urls.py
+++ steganography_api/api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from .routers.ping_route import ping
 from .routers.upload_route import upload_file
 from .routers.encode_route import encode_file
+from .routers.get_file import download_file
 
 urlpatterns = [
     path("ping", ping),
     path("upload", upload_file),
-    path("encode", encode_file)
+    path("encode", encode_file),
+    path("download", download_file)
 ]
