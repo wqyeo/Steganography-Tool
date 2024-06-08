0989fabd4b97d6bc5154a9d9b4bfdc086842e95b
wqyeo <yeo_dev@protonmail.com>
2024-06-08 19:29:54 +0800

    API route to decode png

---
+++ steganography_api/api/controllers/decoders/png_decoder_controller.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+def to_bin(data):
+    if isinstance(data, str):
+        return ''.join([ format(ord(i), "08b") for i in data])
+    elif isinstance(data, bytes) or isinstance(data, np.ndarray):
+        return [ format(i, "08b") for i in data]
+    elif isinstance(data, int) or isinstance(data, np.uint8):
+        return format(data, "08b")
+    else:
+        raise TypeError("Type not supported!")
+
+def _linear_decode_png(image_path, ending_payload, r_bits_usage, g_bits_usage, b_bits_usage):
+    image = cv2.imread(image_path)
+
+    extracted_payload = ""
+    for row in image:
+        for pixel in row:
+            r, g, b = bin(pixel[0])[2:].zfill(8), bin(pixel[1])[2:].zfill(8), bin(pixel[2])[2:].zfill(8)
+
+            # Extract the specified bits from the red, green, and blue channels
+            for r_bit in r_bits_usage:
+                extracted_payload += r[r_bit]
+            for g_bit in g_bits_usage:
+                extracted_payload += g[g_bit]
+
+            for b_bit in b_bits_usage:
+                extracted_payload += b[b_bit]
+    del image
+    
+    decoded_payload = "".join([chr(int(extracted_payload[i:i+8], 2)) for i in range(0, len(extracted_payload), 8)])
+    # Find the ending payload and truncate the extracted payload
+    end_index = decoded_payload.find(ending_payload)
+    key_used = False
+    if end_index != -1:
+        key_used = True
+        decoded_payload = decoded_payload[:end_index]
+
+    return {
+        "found": key_used,
+        "decoded": decoded_payload
+    }
+
+def png_decoder_controller(image_path, ending_payload, r_bits_usage, g_bits_usage, b_bits_usage, generator_type):
+    """
+    Decodes a PNG image to extract hidden payload.
+
+    - image: The encoded image
+    - r_bits_usage: Which bits were used for red during encoding
+    - g_bits_usage: Which bits were used for green during encoding
+    - b_bits_usage: Which bits were used for blue during encoding
+
+    Returns the extracted payload.
+    """
+    return _linear_decode_png(image_path, ending_payload, r_bits_usage, g_bits_usage, b_bits_usage)
---
+++ steganography_api/api/routers/decode_route.py
@@ -0,0 +1,72 @@
+import json
+import magic
+import cv2
+import tempfile
+from pathlib import Path
+import uuid as uuidgen
+
+from django.conf import settings
+from django.core.files import File
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+import os
+
+from ..models import FileModel
+from ..utils.is_valid_file import is_valid_file
+from ..controllers.decoders.png_decoder_controller import png_decoder_controller
+
+def _handle_decode_png_image(decoded_payload):
+    if decoded_payload is None:
+        return JsonResponse({'status': 'ERROR', 'message': 'Failed to decode the image.'}, status=500)
+    return JsonResponse({'status': 'SUCCESS', 'message': 'Decoding is done.', 'found': decoded_payload['found'], 'decoded': decoded_payload['decoded']}, status=200)
+
+@csrf_exempt
+def decode_file(request):
+    if request.method != 'POST':
+        return JsonResponse({'status': 'ERROR'}, status=404)
+
+    try:
+        file_id = request.POST.get("file_uuid")
+        r_bits = json.loads(request.POST.get("r_bits", "[]"))  # Expecting JSON array in string format
+        g_bits = json.loads(request.POST.get("g_bits", "[]"))
+        b_bits = json.loads(request.POST.get("b_bits", "[]"))
+        secret_key = request.POST.get("secret_key")
+        generator_type = request.POST.get("generator_type", "linear")
+
+        # Check if any of these are left empty/blank
+        if not file_id or not secret_key:
+            return JsonResponse({'status': 'ERROR', 'message': 'Required fields are missing.'}, status=400)
+
+        # Validate if all 3 arrays are empty
+        if not r_bits and not g_bits and not b_bits:
+            return JsonResponse({'status': 'ERROR', 'message': 'At least one bit array should be provided.'}, status=400)
+
+        # Function to validate the bits array
+        def validate_bits(bits):
+            if not isinstance(bits, list):
+                return False
+            if not all(isinstance(x, int) and 0 <= x <= 7 for x in bits):
+                return False
+            if len(bits) != len(set(bits)):
+                return False
+            return True
+
+        if not validate_bits(r_bits) or not validate_bits(g_bits) or not validate_bits(b_bits):
+            return JsonResponse({'status': 'ERROR', 'message': 'Bits arrays must be unique integers between 0 and 7.'}, status=400)
+
+        matching_file = get_object_or_404(FileModel, id=file_id)
+
+        # Determine file type, and determine encoding function to use.
+        file_path = matching_file.file.path
+        mime = magic.Magic(mime=True)
+        mime_type = mime.from_file(file_path)
+
+        if mime_type == 'image/png':
+            decoded_payload = png_decoder_controller(file_path, secret_key, r_bits, g_bits, b_bits, generator_type)
+            return _handle_decode_png_image(decoded_payload)
+
+        return JsonResponse({'status': 'ERROR', 'message': 'Unknown error, contact admin!'})
+    except Exception as e:
+        print(f"Error occurred decoding file :: {e}", flush=True)
+        return JsonResponse({'status': 'ERROR'}, status=500)
